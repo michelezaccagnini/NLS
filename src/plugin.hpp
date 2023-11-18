9d79d54394e58ddabab1386d72765d84e4d4ac08
michelezaccagnini <michelezaccagnini@users.noreply.github.com>
2023-11-18 13:19:31 -0500

    fixed manifest

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -10,6 +10,5 @@ extern Model* modelCrossSeq2;
 extern Model* modelTrigGate;
 extern Model* modelCrossSeq3;
 extern Model* modelTrigLogic;
-extern Model *modelGraviHarm;
 // Declare each Model, defined in each module source file
 // extern Model* modelMyModule;
